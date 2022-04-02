1ec234281a577a884246308779671f2131571443
Isthatbeka <91068914+Isthatbeka@users.noreply.github.com>
2022-04-02 13:02:53 +0300

    Update 3 упр.py

--- прак 2/3 упр.py
+++ прак 2/3 упр.py
@@ -7,24 +7,24 @@ y = [16,18,	20,	22,	24]
 sp = plt.subplot(231)
 sp.bar(x, y, color=Кандидаты, edgecolor='k', tick_label=labels)
 sp.grid(True, axis='y')
-plt.title(r'Результаты опросов за месяц до выборов')
-plt.xlabel('Кандидаты')
+plt.title(r'1')
+plt.xlabel('candidates')
 
 x = [1, 2, 3, 4, 5]
 y = [20, 20,	18,	22,	20]
 sp = plt.subplot(232)
 sp.bar(x, y, color=Кандидаты, edgecolor='k', tick_label=labels)
 sp.grid(True, axis='y')
-plt.title(r'Результаты опросов за неделю до выборов')
-plt.xlabel('Кандидаты')
+plt.title(r'2')
+plt.xlabel('candidates')
 
 x = [1, 2, 3, 4, 5]
 y = [24,	22,	20,	18,	16]
 sp = plt.subplot(233)
 sp.bar(x, y, color=Кандидаты, edgecolor='k', tick_label=labels)
 sp.grid(True, axis='y')
-plt.title(r'Экзитполл')
-plt.xlabel('Кандидаты')
+plt.title(r'3')
+plt.xlabel('candidates')
 
 
 
@@ -38,4 +38,8 @@ sp.pie(data, labels=('А', 'Б', 'В', 'Г', 'Д'),colors = ['blue','orange', 'g
 sp = plt.subplot(236)
 data = [24,	22,	20,	18,	16]
 sp.pie(data, labels=('А', 'Б', 'В', 'Г', 'Д'),colors = ['blue','orange', 'green', 'red','purple'])
+print("1-Результаты опросов за месяц до выборов""\n"
+      "2-Результаты опросов за неделю до выборов""\n"
+      "3-'Экзитполл")
+
 plt.show()
